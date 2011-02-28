9996e499cfe68154a75527095e973b0523024ba9
Davi Ferreira <contato@daviferreira.com>
2011-02-28 10:04:04 -0300

    First commit

---
+++ README.markdown
@@ -0,0 +1,7 @@
+Funfou
+======
+
+Site de perguntas e respostas voltado para profissionais de TI.
+---------------------------------------------------------------
+
+Desenvolvido em Rails 3.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Funfou::Application.config.secret_token = '3da2ffdb230cfd1c4a62e2851a2c7aa2de10167f319a98bef28d83857010e1c32c6d142d5da0db706f382b237272c4b3809502cc9d061ca89c78edff9f82ce4a'
