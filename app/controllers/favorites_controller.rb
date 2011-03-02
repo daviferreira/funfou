a39cb96f251657242c8a67dd89c5912efbcc2e75
Davi Ferreira <contato@daviferreira.com>
2011-03-02 15:17:03 -0300

    Favoritewith AJAX

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,14 +2,26 @@ class FavoritesController < ApplicationController
 	before_filter :authenticate
 
 	def create
-		@question = Question.find(params[:favorite][:question_id])
-		current_user.favorite!(@question)
-		redirect_to @question
+		id = params[:favorite][:question_id]
+		if current_user.favorites.find_by_question_id(id).nil?
+			@question = Question.find(id)
+			if @question.user_id != current_user.id
+				current_user.favorite!(@question)
+				respond_to do |format|
+					format.html { redirect_to @question }
+					format.js
+				end
+			end
+		end
 	end
 
 	def destroy
 		@question = Favorite.find(params[:id]).question
 		current_user.unfavorite!(@question)
-		redirect_to @question
+		respond_to do |format|
+			format.html { redirect_to @question }
+			format.js
+		end
 	end
+
 end
