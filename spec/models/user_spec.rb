d4f32000dfb5de42e9c03ccf78135b7e73eae615
daviferreira <contato@daviferreira.com>
2011-03-08 12:15:33 -0300

    User admin initial spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -89,5 +89,24 @@ describe User do
       @user.encrypted_password.should_not be_blank
     end
   end
+  
+  describe "admin attribute" do
+    before(:each) do
+      @user = User.create!(@attr)
+    end
+    
+    it "should respond to admin" do
+      @user.should respond_to(:admin)
+    end
+    
+    it "should not be an admin by default" do
+      @user.should_not be_admin
+    end
+    
+    it "should be convertible to admin" do
+      @user.toggle!(:admin)
+      @user.should be_admin
+    end
+  end
 
 end
