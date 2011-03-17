62ef85a29b881db22eea1236a95555e7a65f8e4e
Davi Ferreira <contato@daviferreira.com>
2011-03-17 14:12:15 -0300

    Favorites spelling

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,7 +1,7 @@
 module QuestionsHelper 
 	def meta_in_words(total, forms)
 		if total == 0
-			"nenhuma #{forms[:singular]}"
+			"sem #{forms[:plural]}"
 		elsif total == 1
 			"1 #{forms[:singular]}"
 		else
