1813269f75531baa34c775ffb2cd759be4be84e6
daviferreira <contato@daviferreira.com>
2011-04-06 16:52:01 -0300

    Fixed a bug on answer voting.

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -2,17 +2,23 @@ class VotesController < ApplicationController
 	before_filter :authenticate
 
   def up    
-    question = Question.find_using_slug(params[:id])
+    @question = Question.find_using_slug(params[:id])
+    @answer = Answer.find(params[:answer_id])
     
-    answer = Answer.find(params[:answer_id])
-    unless answer.user_id == current_user.id
-      answer.score = 0 if answer.score.nil?
-      score = answer.score + 1    
-      recalculate_answers_scores(question)
-      current_user.vote!(question, answer, 1)
-      answer.update_attributes(:score => score)
-    end 
-    redirect_to pergunta_path(question)
+    if @answer.user_id == current_user.id
+      @answer = nil
+      @modified_answer = nil
+    else
+      @modified_answer = recalculate_answers_scores(@question)
+      current_user.vote!(@question, @answer, 1)
+      @answer.reload
+      @answer.update_attributes(:score => @answer.score + 1)
+    end
+    
+    respond_to do |format|
+			format.html { redirect_to pergunta_path(@question) }
+			format.js
+		end
   end
   
   private
@@ -25,13 +31,15 @@ class VotesController < ApplicationController
         answers.each do |answer|
           answers_id += "#{answer.id},"
         end
-        answers_id.sub!(/,$/, '') 
+        answers_id.sub!(/,$/, '')
         votes = Vote.where("user_id = #{current_user.id} AND answer_id IN (#{answers_id})")
         unless votes.empty?
-          vote = votes.first
-          answer = Answer.find(vote.answer_id)
-          answer.update_attributes(:score => answer.score - vote.value)
-          vote.destroy
+          votes.each do |vote|
+            @modified_answer = Answer.find(vote.answer_id)
+            @modified_answer.update_attributes(:score => @modified_answer.score - vote.value)
+            vote.destroy
+          end
+          @modified_answer
         end
       end
     end
