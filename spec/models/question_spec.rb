f421d0db7f48e44cdca2c5a12f0154c8eef692ad
Davi Ferreira <contato@daviferreira.com>
2011-08-04 15:18:11 -0300

    Question model testing

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -2,7 +2,40 @@
 require 'spec_helper'
 
 describe Question do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  before(:each) do
+    @attr = {
+      :title => "Title",
+      :content => "Content"
+    }
+    @user = Factory(:user)
+  end
+
+  it "should create a new question given valid attributes" do
+    @user.questions.create!(@attr)
+  end
+
+  it "should require a title" do
+    no_title_question = @user.questions.new(@attr.merge(:title => ""))
+    no_title_question.should_not be_valid
+  end
+
+  it "should require a content" do
+    no_content_question = @user.questions.new(@attr.merge(:content => ""))
+    no_content_question.should_not be_valid
+  end
+
+  it "should require an user" do
+    question_without_user = Question.new(@attr)
+    question_without_user.should_not be_valid
+  end
+
+  it "should reject titles that are too long" do
+    long_title = "a" * 141
+    long_title_question = @user.questions.new(@attr.merge(:title => long_title))
+    long_title_question.should_not be_valid
+  end
+
 end
 
 # == Schema Information
