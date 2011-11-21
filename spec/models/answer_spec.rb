5e71b516976ffd9118605bfd472bf93ed4043d03
Davi Ferreira <davi.ferreira@corp.globo.com>
2011-11-21 18:35:29 -0200

    Mais testes para o model de resposta.

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -8,17 +8,35 @@ describe Answer do
             :content => "Content",
             :score => 0,
             :published => true,
-            :user_id => @question.user.id,
             :question_id => @question.id
         }
     end
 
     it "should create a new answer given valid attributes" do
         answer = Answer.create(@attr)
-        answer.user_id = @attr[:user_id]
+        answer.user_id = 1 #user_id is not accessible
         answer.should be_valid
-        answer.save!.should be_true
+        lambda do
+            answer.save!.should be_true
+        end.should change(Answer, :count).by(1) 
     end
+
+  it "should require a content" do
+    no_content_answer = Answer.new(@attr.merge(:content => ""))
+    no_content_answer.user_id = 1
+    no_content_answer.should_not be_valid
+  end
+
+  it "should require an user" do
+    answer_without_user = Answer.new(@attr)
+    answer_without_user.should_not be_valid
+  end
+
+  it "should require a question" do
+      answer_without_question = Answer.new(@attr.merge({:question_id => ''}))
+      answer_without_question.should_not be_valid
+  end
+
 end
 
 # == Schema Information
